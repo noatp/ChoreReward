4a46eca300adf5a5207bc60487e25adc01ccb64c
Toan Pham <toan.chpham@gmail.com>
2021-12-12 21:00:27 -0800

    implement readOtherUser

--- ChoreReward/Repository/UserRepository/UserRepository.swift
+++ ChoreReward/Repository/UserRepository/UserRepository.swift
@@ -13,11 +13,11 @@ import CoreMedia
 
 class UserRepository: ObservableObject{
     @Published var currentUser: User? = nil
+    @Published var otherUser: User? = nil
     
     private var currentUserRef: DocumentReference? = nil
     
     private let database = Firestore.firestore()
-    private var currentUserId: String? = nil
     
     init(initCurrentUser: User? = nil){
         self.currentUser = initCurrentUser
@@ -77,6 +77,24 @@ class UserRepository: ObservableObject{
     }
     
     func readOtherUser(otherUserId: String){
-        
+        database.collection("users").document(otherUserId).getDocument{[weak self] (document, error) in
+            let result = Result {
+                try document?.data(as: User.self)
+            }
+            switch result {
+            case .success(let receivedUser):
+                if let otherUser = receivedUser {
+                    // A `user` value was successfully initialized from the DocumentSnapshot.
+                    self?.otherUser = otherUser
+                } else {
+                    // A nil value was successfully initialized from the DocumentSnapshot,
+                    // or the DocumentSnapshot was nil.
+                    print("User does not exist")
+                }
+            case .failure(let error):
+                // A `user` value could not be initialized from the DocumentSnapshot.
+                print("Error decoding user: \(error)")
+            }
+        }
     }
 }
