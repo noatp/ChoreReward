82e09535d888a3d35786ff032cfeb3932838d88d
Toan Pham <toan.chpham@gmail.com>
2022-01-23 10:47:01 -0800

    removed redundancy

--- ChoreReward/ChoreRewardApp.swift
+++ ChoreReward/ChoreRewardApp.swift
@@ -10,41 +10,34 @@ import Firebase
 
 @main
 struct ChoreRewardApp: App {
-    let userService: UserService
-    let familyService: FamilyService
-    let choreService: ChoreService
-    
-    let currentUserRepository: UserRepository
-    let currentFamilyRepository: FamilyRepository
-    let currentChoreRepository: ChoreRepository
     let dependency: Dependency
     
     init(){
         FirebaseApp.configure()
-        self.currentUserRepository = UserRepository()
-        self.currentFamilyRepository = FamilyRepository()
-        self.currentChoreRepository = ChoreRepository()
+        let currentUserRepository = UserRepository()
+        let currentFamilyRepository = FamilyRepository()
+        let currentChoreRepository = ChoreRepository()
         
-        self.userService = UserService(
-            currentUserRepository: self.currentUserRepository
+        let userService = UserService(
+            currentUserRepository: currentUserRepository
         )
-        self.familyService = FamilyService(
-            currentUserRepository: self.currentUserRepository,
-            currentFamilyRepository: self.currentFamilyRepository
+        let familyService = FamilyService(
+            currentUserRepository: currentUserRepository,
+            currentFamilyRepository: currentFamilyRepository
         )
-        self.choreService = ChoreService(
+        let choreService = ChoreService(
             currentUserRepository: currentUserRepository,
             currentFamilyRepository: currentFamilyRepository,
             currentChoreRepository: currentChoreRepository
         )
         
         self.dependency = Dependency(
-            userService: self.userService,
-            familyService: self.familyService,
-            choreService: self.choreService,
-            currentUserRepository: self.currentUserRepository,
-            currentFamilyRepository: self.currentFamilyRepository,
-            currentChoreRepository: self.currentChoreRepository
+            userService: userService,
+            familyService: familyService,
+            choreService: choreService,
+            currentUserRepository: currentUserRepository,
+            currentFamilyRepository: currentFamilyRepository,
+            currentChoreRepository: currentChoreRepository
         )
     }
 
