0e37e07ecc38533ca7fc9d13cc3792b676958458
Toan Pham <toan.chpham@gmail.com>
2022-06-22 19:34:29 -0700

    clean up

--- ChoreReward/UI/Views/ComponentViews/ImageViews/RemoteImageView.swift
+++ ChoreReward/UI/Views/ComponentViews/ImageViews/RemoteImageView.swift
@@ -10,36 +10,18 @@ import Kingfisher
 import SDWebImageSwiftUI
 
 struct RemoteImageView: View {
-//    private let imageUrl: String
-//    private let size: CGSize
-//    private let cachingSize: CGSize
     private let imageUrl: String
     private let isThumbnail: Bool
 
     init(
-//        imageUrl: String,
-//        size: CGSize,
-//        cachingSize: CGSize
         imageUrl: String,
         isThumbnail: Bool
     ) {
-//        self.imageUrl = imageUrl
-//        self.size = size
-//        self.cachingSize = cachingSize
         self.imageUrl = imageUrl
         self.isThumbnail = isThumbnail
     }
 
     var body: some View {
-//        let processor = DownsamplingImageProcessor(size: .init(width: cachingSize.width*3, height: cachingSize.height*3))
-//        return KFImage(URL(string: imageUrl))
-//            .placeholder({ProgressView()})
-//            .setProcessor(processor)
-//            .cancelOnDisappear(true)
-//            .resizable()
-//            .scaledToFill()
-//            .frame(width: size.width, height: size.height)
-
         if isThumbnail {
             WebImage(url: URL(string: imageUrl)!)
                 .resizable()
@@ -65,11 +47,6 @@ struct RemoteImageView: View {
 
 struct RemoteImageView_Previews: PreviewProvider {
     static var previews: some View {
-//        RemoteImageView(
-//            imageUrl: "https://s3.amazonaws.com/brt.org/tim-cook.png",
-//            size: .init(width: 100, height: 100),
-//            cachingSize: .init(width: 100, height: 100)
-//        )
         RemoteImageView(imageUrl: "https://s3.amazonaws.com/brt.org/tim-cook.png", isThumbnail: false)
     }
 }
